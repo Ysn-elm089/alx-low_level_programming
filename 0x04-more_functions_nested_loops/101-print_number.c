1a532da4898eda7c9689e8ba4f7d9486f4f1b251
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2023-06-23 08:40:36 -0700

    flash back

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
+ * print_numbers - prints an integer
  * @n: integer to be printed
+ * Return: Always 0.
  */
-void print_numbers(int n)
+
+void print_number(int n)
 {
 	unsigned int n1;
+
 	if (n < 0)
 	{
 		n1 = -n;
@@ -16,10 +19,11 @@ void print_numbers(int n)
 	{
 		n1 = n;
 	}
+
 	if (n1 / 10)
 	{
 		print_number(n1 / 10);
 	}
-	
+
 	_putchar((n1 % 10) + '0');
 }
