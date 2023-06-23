9fa7457da8e204dcd08b8fe5613dedf350716b9b
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2023-06-22 22:24:56 -0700

    play the game

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - uppercase letter 
+ * _isupper - uppercase letter
  * @c: char to check
  *
  * Return: 0 or 1
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -30,7 +30,7 @@ int main(void)
 		{
 			printf("%d", i);
 		}
-		else 
+		else
 		{
 			printf(" %d", i);
 		}
