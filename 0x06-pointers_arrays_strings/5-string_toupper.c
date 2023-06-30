8b9f966915960758cf04722856604144fe66a928
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2023-06-30 11:02:23 -0700

    let see

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_troupper - changes all lowcase letters
+ * of a string to uppercase.
+ * @str: the string to be changed.
+ * Return: A pointer to the changes string.
+ *
+ */
+char *string_toupper(char *str)
+
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+		str[index] -= 32;
+		index++;
+	}
+	return (str);
+}
