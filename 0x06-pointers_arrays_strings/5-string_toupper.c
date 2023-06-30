cd8f18c2dc17423fa7632bcdd7a2f1aa764b2085
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2023-06-30 11:57:03 -0700

    goo

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,21 +2,23 @@
 
 /**
  * *string_troupper - changes all lowcase letters
- * of a string to uppercase.
+ * to uppercase letters
  * @str: the string to be changed.
- * Return: A pointer to the changes string.
+ * Return: a pointer to the changed string.
  *
  */
-char *string_toupper(char *str)
 
+char *string_toupper(char *str)
 {
 	int index = 0;
 
 	while (str[index])
 	{
 		if (str[index] >= 'a' && str[index] <= 'z')
-		str[index] -= 32;
-		index++;
+		{
+			str[index] -= 32;
+			index++;
+		}
 	}
 	return (str);
 }
