cfa2ad0b137a8af3f8f8a1ce5c15079f43827dd4
Ysn-elm089 <yassine.elmahdaoui89@gmail.com>
2023-07-09 22:48:44 -0700

    go

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings
+ * using n as number of bytes from scr
+ * @dest: string uppon
+ * @src: string dest
+ * @n: number of bytes from scr to dest.
+ * Return: pointer to resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
